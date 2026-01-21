afd87196fabbc94bff6715e989be4d3da9e3fe59
owenholb-coder <owenholb@gmail.com>
2026-01-21 18:36:14 +0000

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,4 +1,6 @@
 const SUPABASE_URL = "https://ookytzrplwzpqpqujhrk.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_4Q-bAS-MmQeBDN3PCkzLBg_MTzShG0V";
 
-window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.sb = sb;
